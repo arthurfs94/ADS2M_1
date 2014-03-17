0aae5e3476cd5574c7f446dd085121c7206142c1
Arthur <Arthurfs94@gmail.com>
2014-03-16 22:05:03 -0300

    trabalho t2 orientado objeto

---
+++ Orientado Objeto/src/com/senac/pessoa/View.java
@@ -0,0 +1,20 @@
+package com.senac.pessoa;
+import java.util.Random;
+
+public class View {
+	
+	public static void main(String args []){
+		
+		Random p = new Random();
+		int a;
+		
+		for(int i=0; i<=10; i++);{
+			a = p.nextInt(50);
+			System.out.println(a + " ");
+			
+			
+			}
+			
+		}
+
+}
