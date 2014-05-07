14474c28a22287b36622167d0c6fcee24ce817db
arthurfs94 <arthurfs94@gmail.com>
2014-05-07 11:32:35 -0300

    Prova
    test posicao e combustivel do tanque.

---
+++ Prova/src/com/senac/jogo/Comandos.java
@@ -0,0 +1,19 @@
+package com.senac.jogo;
+
+public class Comandos {
+
+	public int posicao;
+	String aux;
+	public int tanque;
+	
+	int[] rua = new int[11];
+	int[] caminho = new int[11];
+	
+	public void Posicao() {
+		posicao = 0;
+	}
+	public void tanque() {
+		tanque = 6;
+	}
+
+}
---
+++ Prova/src/com/senac/jogo/test/testTest.java
@@ -0,0 +1,34 @@
+package com.senac.jogo.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.senac.jogo.Comandos;
+
+public class testTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testPosicao() {
+		Comandos comd = new Comandos();
+		assertTrue(comd.posicao == 0);
+	}
+	@Test
+	public void testTanque() {
+		Comandos comd = new Comandos();
+		assertTrue(comd.tanque == 6);
+	}
+
+
+
+}
